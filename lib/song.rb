9efac47f602f180f36c2bf4c0cc3b964b8742c4e
Kameron Hungerford <kam.hungerford@gmail.com>
2020-08-01 21:32:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,19 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist, :genre
   
-  
   @@count = 0
-  @@genres = []
   @@artists = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artists << @artist
-    @@genres << @genre
+    @@artists << artist
+    @@genres << genre
   end
   
   def self.count
@@ -20,34 +21,31 @@ class Song
   end
   
   def self.artists
-    @@artists.uniq
+    @@artists.uniq!
   end
   
   def self.genres
-    @@genres.uniq
+    @@genres.uniq!
   end
   
   def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
+    @@genres.each_with_object(Hash.new) do |genre, new_hash|
+      if new_hash[genre]
+         new_hash[genre] += 1
+       else
+         new_hash[genre] = 1
       end
     end
-    genre_count
   end
   
   def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
+    @@artists.each_with_object(Hash.new) do |artist, new_hash|
+      if new_hash[artist]
+        new_hash[artist] += 1
       else
-        artist_count[artist] = 1
+        new_hash[artist] = 1
       end
     end
-    artist_count
   end
 end
+
